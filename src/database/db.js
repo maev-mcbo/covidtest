5bea88a33905c2acf0570f8a401460ae8520a350
Mario Echeverria <soyox2037@gmail.com>
2022-04-02 09:00:21 -0400

    commit

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const clientDB = mongoose
 
-.connect('mongodb+srv://main-user:vNrpLaSSgpTC1Fb3@cluster0.fe3nl.mongodb.net/coviddb') 
+.connect(`${process.env.URI}`) 
     .then((m) => {
         console.log('Conectado a la Base de Datos ')
         return m.connection.getClient();
